cef8d4b0da8c47c8f835480e1e3bcb27fbe56fde
AymaraCabanl <aymicabanal@hotmail.com>
2025-09-28 10:01:26 -0300

    actualizo

--- views/viewResponse.js
+++ views/viewResponse.js
@@ -1,24 +1,35 @@
 const LibraryView = {                   //La vista recibe los datos crudos del modelo por medio del controlador y los convierte en un formato que se le pueda enviar al cliente.
     formatResponse : (data) => {        //Por medio del parametro data es que recibe la información del comando que haya elegido el cliente.
         if(!data){                      //Manejamos los errores por si es un comando no reconocido o null.
-            return JSON.stringify({
-                status: "error",
-                message: "❌ No se pudo realizar la operación."
-            }, null, 2)
-        }
-            return JSON.stringify({
+        return JSON.stringify({
+            status: "error",
+            message: "❌ No se pudo realizar la operación."
+        });
+    }
+
+    if(typeof data === 'string'){        //Por si es un mensaje de texto, lo muestra así
+        return JSON.stringify({
             status: "success",
             data
-
-        }, null, 2);
+        });
     }
     
-};
+    if(Array.isArray(data)){            //Si es un array, lo muestra como un array de objetos
+        return JSON.stringify({
+            status: "success",
+            data
+        });
+    }
+
+    return JSON.stringify({             //Si es un único libro, lo envía como objeto.
+        status: "success",
+        data
+    });
+    
+    },
+}
 
 export default LibraryView;
-
-
-
 
 
 
